d4a2b5d74ee7f0f8c7a65a83bc3f31b7484d2cf9
Kashif <ahmed.kashif1001@gmail.com>
2024-04-21 23:50:10 +0500

    middlewares for express and app

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import express from 'express'
 
 const app = express() //this is the same as saying createServer in normal nodejs
 
+app.get(express.json())
+
 app.get('/',(req,res) => {
     res.status(200).json({"msg":"Hello User!"})
 })
@@ -35,6 +37,16 @@ app.listen(4000, 'localhost', ()=>console.log("Listening to port 4000"))
 
 //THIS WAS ALL ABOUT RESPONDING AND NOT GETTING ANYTHING FROM THE USER
 
+//WE WOULD USE THE REQUEST BODY TO RECEIVE THE DATA IN CASE OF FORM AND POSTS EXAMPLE. AS YOU SEE IN THE POSTMAN WE SEND THE DATA 
+//USING RESPONSE BODY SO THE ACTUAL DATA IS INSIDE THE BODY OF A REQ OR RES
+
+//
+//
+// middleswares can be seen as checkpoints between a client and a server for example when we request a webpage, the middleware comes in the middle and says is this request something that I want/can handle then its going to respond with it and then end the response or go to the next one if it is specified to go to the next one so for our project we would create our own middleware
+//
+//
+//
+//
 
 
 
