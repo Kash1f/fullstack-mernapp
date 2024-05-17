76e760ba49cabee5fdaa84975ea32f8702a59834
Kashif <ahmed.kashif1001@gmail.com>
2024-05-17 23:18:44 +0500

    layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,16 @@
-
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Layout from './pages/Layout'
 
 function App() {
   
 
   return (
-    <>
-    Hello
-      </>
+   <BrowserRouter>
+   <Routes>
+    <Route path='/' element={<Layout/>}>
+      </Route>
+   </Routes>
+   </BrowserRouter>
   )
 }
 
---
+++ frontend/src/pages/Layout.jsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom"
+
+
+const Layout = () => {
+  return (
+    <>
+    <header>
+        <nav>
+            <Link to="/">Home</Link>
+        </nav>
+    </header>
+    </>
+  )
+}
+
+export default Layout
